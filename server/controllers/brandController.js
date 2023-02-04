80162f207e84e72fa7f6e0ec06b8d6e4465b6a86
Greg Kovshov <barkev2009@bk.ru>
2023-02-04 15:52:23 +0700

    added start.sh

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,10 +1,17 @@
+const {Brand} = require('../models/models');
+const ApiError = require('../error/ApiError');
+
 class BrandController {
     async create(req, resp) {
+        const {name} = req.body;
+        const brand = await Brand.create({name})
 
+        return resp.json(brand);
     } 
 
     async getAll(req, resp) {
-        
+        const brands = await Brand.findAll();
+        return resp.json(brands);
     } 
 }
 
--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,10 +1,17 @@
+const {Type} = require('../models/models');
+const ApiError = require('../error/ApiError')
+
 class TypeController {
     async create(req, resp) {
+        const {name} = req.body;
+        const type = await Type.create({name});
 
+        return resp.json(type);
     } 
 
     async getAll(req, resp) {
-        
+        const types = await Type.findAll();
+        return resp.json(types);
     } 
 }
 
